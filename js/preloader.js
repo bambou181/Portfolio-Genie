cdcb1133870b910fbd26b4dbb89d61ac1893e74f
bambou181 <patrickmouroufie56@gmail.com>
2023-11-06 12:47:48 +0000

    cv master portfolio

---
+++ js/preloader.js
@@ -0,0 +1,35 @@
+//no used this code for preloader's animation
+
+"use strict";
+jQuery( document ).ready( function ( $ ) {
+
+//for Preloader
+
+    $( window ).load( function () {
+        $( "#loading" ).fadeOut( 500 );
+    } );
+
+
+   
+
+
+
+    
+
+
+
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
